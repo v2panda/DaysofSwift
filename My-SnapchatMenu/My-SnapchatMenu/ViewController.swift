7fe9c18c48ca8f69e4cb4375163a5eaec0575ac1
v2panda <pdxuzhen@gmail.com>
2016-02-19 10:54:53 +0800

    add My-SnapchatMenu

---
+++ My-SnapchatMenu/My-SnapchatMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My-SnapchatMenu
+//
+//  Created by Panda on 16/2/19.
+//  Copyright © 2016年 v2panda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
