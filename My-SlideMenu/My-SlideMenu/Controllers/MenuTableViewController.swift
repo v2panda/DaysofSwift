f6869bcfebc6fd0a4edbd93e73b9a39a3a5e8f6c
v2panda <pdxuzhen@gmail.com>
2016-03-02 23:21:53 +0800

    update SlideMenu

--- My-SlideMenu/My-SlideMenu/Controllers/MenuTableViewController.swift
+++ My-SlideMenu/My-SlideMenu/Controllers/MenuTableViewController.swift
@@ -54,8 +54,7 @@ class MenuTableViewController: UITableViewController {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        print("menu")
         
         let menuTableViewController = segue.sourceViewController as! MenuTableViewController
         
--- My-SlideMenu/My-SlideMenu/Controllers/NewsTableViewController.swift
+++ My-SlideMenu/My-SlideMenu/Controllers/NewsTableViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class NewsTableViewController: UITableViewController {
+class NewsTableViewController: UITableViewController ,TransitionManagerDelegate{
 
+    let menuTransitionManager = TransitionManager()
+    
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent
     }
@@ -18,6 +20,7 @@ class NewsTableViewController: UITableViewController {
         super.viewDidLoad()
         self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None
         self.view.backgroundColor = UIColor(red:0.062, green:0.062, blue:0.07, alpha:1)
+        self.title = "Everyday Moments"
     }
 
     override func didReceiveMemoryWarning() {
@@ -65,51 +68,30 @@ class NewsTableViewController: UITableViewController {
         return cell
     }
 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    @IBAction func unwindToNews(segue:UIStoryboardSegue) {
+        let menuController = segue.sourceViewController as! MenuTableViewController
+        self.title = menuController.currentItem
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    // MARK: TransitionManagerDelegate
+    func transitionManagerDismiss() {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        print("233")
         
+        let menuController = segue.destinationViewController as! MenuTableViewController
+        menuController.currentItem = self.title!
+        menuController.transitioningDelegate = menuTransitionManager
+        menuTransitionManager.delegate = self
         
+
     }
 
 
